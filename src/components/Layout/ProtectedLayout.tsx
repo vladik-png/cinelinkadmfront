1dbe8b1810b7f2b032679a8e25cb0b6f4134ecab
vladik-png <vladsemotuk42@gmail.com>
2026-05-25 16:24:13 +0300

    fix terminal page

--- src/components/Layout/ProtectedLayout.tsx
+++ src/components/Layout/ProtectedLayout.tsx
@@ -14,12 +14,12 @@ export const ProtectedLayout: React.FC = () => {
     return (
         <div className="flex w-full min-h-screen bg-[#151521] text-slate-200">
             <Sidebar />
-            <div className="flex-1 ml-64 min-h-screen flex flex-col relative">
-                <div className="sticky top-0 z-40 w-full">
+            <div className="flex-1 ml-64 min-h-screen flex flex-col relative min-w-0">
+                <div className="sticky top-0 z-40 w-full min-w-0">
                     <Header />
                 </div>
 
-                <main className="flex-1 w-full flex flex-col">
+                <main className="flex-1 w-full flex flex-col min-w-0">
                     <Outlet />
                 </main>
             </div>
